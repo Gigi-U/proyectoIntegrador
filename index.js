5364527e491d192cb1772c706d5fad23bfc67b56
Gigi-U <andanlibres@gmail.com>
2023-04-28 19:01:00 -0300

    cambios en texti lugares

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ const listadoEventos = [
     img: "/assets/img/eventImages/music01.png",
     titulo: "Música a Cielo Abierto",
     fecha:"02/03/2024",
-    lugar:"santa fe",
+    lugar:"Santa Fe",
     descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
   },
   {
     img: "/assets/img/eventImages/kids01.png",
     titulo: "Festival de Verano",
     fecha:"02/03/2024",
-    lugar:"gba",
+    lugar:"GBA",
     descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
   },
   {
     img: "/assets/img/eventImages/gastro01.png",
     titulo: "Picnic Primaveral",
     fecha:"02/03/2024",
-    lugar:"caba",
+    lugar:"CABA",
     descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
   },
   {
     img: "/assets/img/eventImages/amusement01.png",
     titulo: "Parque de Diversiones",
     fecha:"02/03/2024",
-    lugar:"cordoba",
+    lugar:"Córdoba",
     descripcion: "Lorem ipsum dolor sit amet consectetur adipisicing elit...",
   }  
 ];
